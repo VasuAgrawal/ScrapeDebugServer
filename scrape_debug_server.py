d11fa58c2c70cb53c740f11567ebf7d12c112706
Vasu Agrawal <vasu_agrawal@yahoo.com>
2023-01-07 17:24:39 -0500

    Initial working version

---
+++ scrape_debug_server.py
@@ -0,0 +1,121 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from werkzeug.datastructures import ImmutableMultiDict, EnvironHeaders
+import pprint
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    props = [
+        "_cached_json",
+        # "_get_file_stream",
+        # "_get_stream_for_parsing",
+        # "_load_form_data",
+        # "_parse_content_type",
+        "accept_charsets",
+        "accept_encodings",
+        "accept_languages",
+        "accept_mimetypes",
+        "access_control_request_headers",
+        "access_control_request_method",
+        "access_route",
+        # "application",
+        "args",
+        "authorization",
+        "base_url",
+        "blueprint",
+        "blueprints",
+        "cache_control",
+        "charset",
+        # "close",
+        "content_encoding",
+        "content_length",
+        "content_md5",
+        "content_type",
+        "cookies",
+        "data",
+        "date",
+        # "dict_storage_class",
+        "encoding_errors",
+        "endpoint",
+        "environ",
+        "files",
+        "form",
+        # "form_data_parser_class",
+        # "from_values",
+        "full_path",
+        # "get_data",
+        # "get_json",
+        "headers",
+        "host",
+        "host_url",
+        "if_match",
+        "if_modified_since",
+        "if_none_match",
+        "if_range",
+        "if_unmodified_since",
+        "input_stream",
+        "is_json",
+        "is_multiprocess",
+        "is_multithread",
+        "is_run_once",
+        "is_secure",
+        # "json",
+        "json_module",
+        # "list_storage_class",
+        # "make_form_data_parser",
+        "max_content_length",
+        "max_form_memory_size",
+        "max_forwards",
+        "method",
+        "mimetype",
+        "mimetype_params",
+        # "on_json_loading_failed",
+        "origin",
+        # "parameter_storage_class",
+        "path",
+        "pragma",
+        "query_string",
+        "range",
+        "referrer",
+        "remote_addr",
+        "remote_user",
+        "root_path",
+        "root_url",
+        "routing_exception",
+        "scheme",
+        "script_root",
+        "server",
+        "shallow",
+        "stream",
+        "trusted_hosts",
+        "url",
+        "url_charset",
+        "url_root",
+        "url_rule",
+        "user_agent",
+        # "user_agent_class",
+        "values",
+        "view_args",
+        "want_form_data_parsed",
+    ]
+
+    output = {}
+    for p in props:
+        try:
+            out = getattr(request, p)
+
+            if isinstance(out, ImmutableMultiDict) or isinstance(out, EnvironHeaders):
+                out = { k: out.getlist(k) for k in out.keys() }
+
+            output[f"{p}: {'.'.join([type(out).__module__, type(out).__qualname__])}"] = f"""{pprint.pformat(out)}"""
+            # print(p)
+            # print(output[p])
+            # print("\n\n")
+        except Exception as e:
+            output[f"EXCEPTION: {p}"] = str(e)
+
+    return render_template('response.html', output = output)
